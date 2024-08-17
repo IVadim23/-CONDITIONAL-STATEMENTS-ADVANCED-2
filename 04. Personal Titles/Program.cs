44718f003bcd5e82e78b4ea6b13d53ba596cc20d
IVadim23 <inazrski@gmail.com>
2024-08-18 01:02:16 +0300

    Add files via upload

---
+++ 04. Personal Titles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            //1. проверка за пола
+            if (gender == "m")
+            {
+                //проверка за възрастта
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else   //age >= 16
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+            else if (gender == "f")
+            {
+                //проверка за възрастта
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else  //age >= 16
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+        
+       } 
+        }
+    }
