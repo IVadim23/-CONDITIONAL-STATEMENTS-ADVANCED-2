3c67c68c3a5f3f01882e088bdc9461729f878b29
IVadim23 <inazrski@gmail.com>
2024-08-18 01:06:51 +0300

    Add files via upload

---
+++ 06. Number in Range/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06._Number_in_range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            //1. [-100; 100] -> >= -100 и <= 100
+            //2. != 0
+            if (number >= -100 && number <= 100 && number != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
