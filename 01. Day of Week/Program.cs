48c97af47253895c64fed45271d5a2af175bd70c
IVadim23 <inazrski@gmail.com>
2024-08-18 00:57:33 +0300

    Add files via upload

---
+++ 01. Day of Week/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            //1 -> Monday
+            //2 -> Tuesday
+            //3 -> Wednesday
+            //4 -> Thursday
+            //5 -> Friday
+            //6 -> Saturday
+            //7 -> Sunday
+            //other -> Error
+
+            //switch -> серия за проверки за 1 променлива само за точни стойности
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
