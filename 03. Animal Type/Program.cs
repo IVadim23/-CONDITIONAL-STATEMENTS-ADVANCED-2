b129f04924bd12c2b48dc19796528f897ee8740f
IVadim23 <inazrski@gmail.com>
2024-08-18 00:59:58 +0300

    Add files via upload

---
+++ 03. Animal Type/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03._Animal_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            //dog -> mammal
+            //crocodile, tortoise, snake -> reptile
+            //other -> unknown
+
+            //switch -> серия от проверки за точни стойности за променлива animal
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }    }
+    }
+}
