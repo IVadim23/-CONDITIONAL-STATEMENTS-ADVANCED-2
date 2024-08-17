662f194724b39e23fe5faa23d9dbe8d82f1fe722
IVadim23 <inazrski@gmail.com>
2024-08-18 01:11:11 +0300

    Add files via upload

---
+++ 10. Invalid Number/Program.cs
@@ -0,0 +1,29 @@
+﻿
+  using System;
+
+
+namespace _10._Invalid_Number
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int number = int.Parse(Console.ReadLine());
+                //ВАЛИДНО -> [100…200] или е 0 -> печатаме нищо
+                //НЕВАЛИДНО -> ПЕЧАТАМ invalid
+
+                bool isValidNumber = (number >= 100 && number <= 200) || number == 0;
+                //true -> валидно
+                //false -> невалидно
+
+                if (!isValidNumber)
+                {
+                    Console.WriteLine("invalid");
+                }
+
+            }
+        }
+   }
+
+    
+
