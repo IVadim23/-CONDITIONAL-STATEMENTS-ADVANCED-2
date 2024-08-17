21c208a4f8661a999d2ddb04f1dd49c976f1c180
IVadim23 <inazrski@gmail.com>
2024-08-18 00:58:49 +0300

    Add files via upload

---
+++ 02. Weekend or Working Day/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Weekend_or_Working_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            //Monday -> Working day
+            //Tuesday -> Working day
+            //Wednesday -> Working day
+            //Thursday -> Working day
+            //Friday -> Working day
+            //Saturday -> Weekend
+            //Sunday -> Weekend
+            //other -> Error
+
+            //switch -> серия от проверки за точни стойности на 1 променлива
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine("Weekend");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
